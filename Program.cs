8e4c5519d6954a1594f2a5cf63baa39023459d96
Azim <azimsadikov@gmail.com>
2017-07-07 15:35:47 -0500

    lists exersize is done

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise
+            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+            List<string> planetList1 = new List<string>(){"Pluton", "Neptun"};
+            planetList.AddRange(planetList1);
+            planetList.Insert(2, "Earth");
+            planetList.Insert(4, "Venus");
+            planetList.Add("Pluto");
+            string[] output = planetList.GetRange(2, 2).ToArray();
+            planetList.RemoveAt(8);
+        }
+    }
+}
